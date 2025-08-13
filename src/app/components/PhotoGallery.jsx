c474f64ef658106906f81de0363ab079376c194b
Siddharth <siddharthisrani123@gmail.com>
2025-08-13 23:47:54 +0530

    okk3

--- src/app/components/PhotoGallery.jsx
+++ src/app/components/PhotoGallery.jsx
@@ -11,8 +11,12 @@ import 'swiper/css/pagination'
 export default function PhotoGallery({ onNext }) {
 
     const photos = [
-        { id: 1, src: "/images/1.jpg" },
-        { id: 2, src: "/images/2.jpg" },
+        { id: 1, src: "/images/2.jpeg" },
+        { id: 2, src: "/images/1.jpeg" },
+        { id: 3, src: "/images/3.jpeg" },
+        { id: 4, src: "/images/4.jpeg" },
+        { id: 5, src: "/images/5.jpeg" },
+        { id: 6, src: "/images/6.jpeg" },
     ]
 
     return (
